9d117534e115aa118c2e3c90666e4fe0c78c5922
Andreas Baumgartner <andreas.baumgartner@simedia.com>
2022-01-11 14:19:38 +0100

    convert Vue3 app to web component

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
+import { defineCustomElement } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+customElements.define("test-project", defineCustomElement(App));
